ec88ce36c480b2a7e3dddd9274440e3d24204ec2
eliasjp <illumine1998@gmail.com>
2023-04-19 21:28:24 -0300

    create function for get and patch users by ids

--- users/permissions.py
+++ users/permissions.py
@@ -2,8 +2,11 @@ from rest_framework.permissions import IsAdminUser, BasePermission
 
 class IsAdminUser(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_employee)
-    
-class IsOwner(BasePermission):
+        return bool(request.user and request.user.is_employee)    
+
+class IsOwnerOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(request.user and request.user.is_employee)
+        if request.user.is_authenticated and request.user != obj:
+            return request.user.is_employee
+        elif request.user.is_authenticated and request.user == obj:
+            return True
